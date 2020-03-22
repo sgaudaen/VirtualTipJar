2cb2fb95027f68efc2f62c0d8bb80f7fd486e9d5
Steven Gaudaen <sgaudaen@hammers.com>
2020-03-22 19:25:21 -0400

    Add site styles and move some elements arround

--- index.php
+++ index.php
@@ -12,16 +12,18 @@
     ?>
 
     <h1>We can't go to the bar. But we can still tip our bartenders.</h1>
-    <p>When you grab a drink out of the fridge, hit this button and tip the bartender.</p>
 
     <?php $r = get_random_restaurant(); ?>
 
     <div class="entry">
-        <h2><?php echo "$r[1], $r[0]"; ?></h2>
-        <p><?php echo "VENMO -- $r[2] | CASHAPP -- $r[3] | PAYPAL -- $r[4]";?></p>
-        <button onClick="window.location.reload();">Give Me Another</button>
+        <h2><span><?php echo "$r[1], $r[0]"; ?></span></h2>
+        <p><span><?php echo "VENMO -- $r[2]";?></span></p>
+        <p><span><?php echo "CASHAPP -- $r[3]";?></span></p>
+        <p><span><?php echo "PAYPAL -- $r[4]";?></span></p>
     </div>
 
+    <button onClick="window.location.reload();">Give Me Another</button>
+    
 <footer><p>Hat tip to the local heroes who made this virtual tip jar spreadsheet. I think it was Ana Owens and Katie Gentsch.
 <a href="mailto:maggie@januarythird.co">Write me</a> with questions, comments, additions.</p></footer>
 </body>
