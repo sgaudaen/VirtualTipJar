1e3b3b96295e5ab12c5a6d1669e19ac957660214
Steven Gaudaen <sgaudaen@hammers.com>
2020-03-22 15:57:16 -0400

    Added generic html stuff

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
-<?php  
-    require 'functions.php';    
-?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Virtual Tip Jar</title>
+  <meta name="description" content="The HTML5 Herald">
+  <link rel="stylesheet" href="site.css">
+</head>
 
-<h1>We can't go to the bar. But we can still tip our bartenders.</h1>
-<p>When you grab a drink out of the fridge, hit this button and tip the bartender.</p>
+<body>
+    <?php  
+        require 'functions.php';    
+    ?>
 
-<?php $r = get_random_restaurant(); ?>
+    <h1>We can't go to the bar. But we can still tip our bartenders.</h1>
+    <p>When you grab a drink out of the fridge, hit this button and tip the bartender.</p>
 
-<div class="entry">
-    <h2><?php echo "$r[1], $r[0]"; ?></h2>
-    <p><?php echo "VENMO -- $r[2] | CASHAPP -- $r[3] | PAYPAL -- $r[4]"; ?></p>
-    <button onClick="window.location.reload();">Refresh Page</button>
-</div>
+    <?php $r = get_random_restaurant(); ?>
+
+    <div class="entry">
+        <h2><?php echo "$r[1], $r[0]"; ?></h2>
+        <p><?php echo "VENMO -- $r[2] | CASHAPP -- $r[3] | PAYPAL -- $r[4]"; ?></p>
+        <button onClick="window.location.reload();">Refresh Page</button>
+    </div>
+</body>
+
+</html>
