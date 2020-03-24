6d3a431e441bf7f9a602e4ca0400d91028054569
Alex Barbato <alex.j.barbato@gmail.com>
2020-03-23 22:55:38 -0400

    Should be a mostly there mobile version

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-161432750-1"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
     gtag('js', new Date());
 
     gtag('config', 'UA-161432750-1');
@@ -25,23 +28,9 @@
     <section class="main-copy">
         <h1>We can't go to the bar. <u>But we can still tip.</u></h1>
 
-        <footer>
-            <div>
-                <p>Add yourself to the spreadsheet <a href="https://forms.gle/j1Lni9pJ6RKTFENG9">here</a>.</p>
-                <p>Amazing spreadsheet by <a
-                        href="https://docs.google.com/spreadsheets/u/3/d/1tz2uyhgy3MsBS68MHPzO8H455_879fqfIaRPyUUw3QE/htmlview#gid=0">Ana
-                        Owens</a>.</p>
-                <p>Design & Development by <a href="https://www.januarythird.co">January Third</a>.</p>
-                <p class="questions">Questions? Bugs? <a href="maggie@januarythird.co">Email us!</a></p>
-            </div>
-
-            <svg width="31" height="23" xmlns="http://www.w3.org/2000/svg">
-                <g fill="#FFF" fill-rule="evenodd">
-                    <path
-                        d="M4 6L1.649 7.236l.449-2.618L.196 2.764l2.628-.382L4 0l1.176 2.382 2.628.382-1.902 1.854.45 2.618zM15 6l-2.351 1.236.449-2.618-1.902-1.854 2.628-.382L15 0l1.176 2.382 2.628.382-1.902 1.854.45 2.618zM27 6l-2.351 1.236.449-2.618-1.902-1.854 2.628-.382L27 0l1.176 2.382 2.628.382-1.902 1.854.45 2.618zM.28 10.78h28.91v3.92H.28zM.28 18.62h28.91v3.92H.28z" />
-                </g>
-            </svg>
-        </footer>
+        <div class="no-mobile">
+            <?php include("footer.html"); ?>
+        </div>
     </section>
 
     <section class="content">
@@ -68,6 +57,10 @@
                 </svg></button>
         </div>
     </section>
+
+    <div class="mobile-only">
+        <?php include("footer.html"); ?>
+    </div>
 </body>
 
 </html>
