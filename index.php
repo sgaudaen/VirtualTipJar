65f6f05f5ffaacdabeac2dfe3d9b37742b2d6891
Steven Gaudaen <sgaudaen@hammers.com>
2020-03-24 22:18:01 -0400

    More styling changes

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         require 'functions.php';    
     ?>
     <div class="left-block">
-        <h1>We can't go to the bar. <span class="underline">But we can still tip our bartenders.</span></h1>
+        <h1>We can't go to the bar. <span class="underline">But we can still tip.</span></h1>
     </div>
 
     <div class="right-block">
@@ -31,22 +31,25 @@
                 <h3>From <?php echo "$r[0]"; ?> —</h3>
                 <h2><span><?php echo "$r[1]"; ?></span></h2>
                 <div class="payment-container">
-                    <?php if(!empty($r[2])){ echo "<p><span>VENMO $r[2]</span></p>";} ?>
-                    <?php if(!empty($r[3])){ echo "<p><span>CASHAPP $r[3]</span></p>";} ?>
-                    <?php if(!empty($r[4])){ echo "<p><span>PAYPAL $r[4]</span></p>";} ?>
+                    <?php if(!empty($r[2])){ echo "<p><span class='pay-type'>VENMO</span> <span class='underline'>$r[2]</span></p>";} ?>
+                    <?php if(!empty($r[3])){ echo "<p><span class='pay-type'>CASHAPP</span> <span class='underline'>$r[3]</span></p>";} ?>
+                    <?php if(!empty($r[4])){ echo "<p><span class='pay-type'>PAYPAL</span> <span class='underline'>$r[4]</span></p>";} ?>
                 </div>
-                <button onClick="window.location.reload();">Find Another</button>
+                <button class="button" onClick="window.location.reload();">Find Another &rarr;</button>
+            </div>
+            <div class=link-container>
+                <a href="https://forms.gle/j1Lni9pJ6RKTFENG9">Add yourself</a>
+                <a href="https://docs.google.com/spreadsheets/d/17cWaeBUNaAM_N5qluK6lcusTx0_GWnrQhm8tbHs5dR0/edit#gid=0">Log Your Tips</a>
             </div>
         </div>
+         
     </div>
 
     <footer>
-            <p>Data from <a href="https://docs.google.com/spreadsheets/u/3/d/1tz2uyhgy3MsBS68MHPzO8H455_879fqfIaRPyUUw3QE/htmlview#gid=0">this spreadsheet</a> by Amy Owens and Katie Gentsch. </br>
-            Add yourself to the spreadsheet <a href="https://forms.gle/j1Lni9pJ6RKTFENG9">here.</a></br> 
-            Design by <a href="https://januarythird.co">January Third</a>. (Very much WIP, but hey, it works!)</br>
-            Development by Steven Gaudaen and Alex Barbato.
-            Make your own version — source code <a href="https://github.com/sgaudaen/VirtualTipJar">here.</a></br>
-            <span class="red-span">Questions? Bugs? <a href="mailto:maggie@januarythird.co">Email us.</a> And don't forget to tip generously.</span>
+            <p>Data pulled from <a href="https://docs.google.com/spreadsheets/u/3/d/1tz2uyhgy3MsBS68MHPzO8H455_879fqfIaRPyUUw3QE/htmlview#gid=0">the spreadsheet</a> by Amy Owens and Katie Gentsch. </br>
+            Design by <a href="https://januarythird.co">January Third</a> Development by Steven Gaudaen and Alex Barbato.</br>
+            Make your own version — <a href="https://github.com/sgaudaen/VirtualTipJar">source code</a>. </br>
+            <span class="red-span">Questions? Bugs? <a href="mailto:maggie@januarythird.co">Email us.</a> And tip generously!</span>
             </p>
     </footer>
 </body>
